044e1fa72ea9dd58ee069a365828d327db3d8bea
shrinivedhaa <shrinivedhaa@gmail.com>
2025-02-25 17:00:38 +0530

    Parent Child Products

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../../entity/product';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-child',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
   @Input() message!: string;
   @Output() notifyParent = new EventEmitter<string>();
+  @Input() selectedProduct?: Product;
 
   sendMessage(){
     this.notifyParent.emit('Message from Child!');
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChildComponent } from '../child/child.component';
+import { Product } from '../../entity/product';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-parent',
-  imports: [ChildComponent],
+  imports: [ChildComponent, CommonModule],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit{
   parentMessage = 'Hello from Parent!';
   childMessage = '';
+  products:Product[]=[];
+  product?:Product;
 
+  constructor(private productService: ProductService){}
+  selectProduct(product:Product){
+    this.product=product;
+  }
   receiveMessage(message:string){
     this.childMessage = message;
+  }
+
+  ngOnInit(): void {
+    this.products=this.productService.getAllProducts();
   }
 }
